27c59d62476483f81e1fffb179e2656b68874404
Matt Fehskens <matt.fehskens@gmail.com>
2016-05-17 20:22:52 -0400

    Added `git init` to initial script

--- scripts/initial.js
+++ scripts/initial.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const childProcess = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
@@ -38,6 +39,7 @@ const askQuestions = (questions, answers) => {
 const processAnswers = (answers) => {
     createNewPackageJson(answers.repoName, answers.repoUrl);
     createNewReadme(answers.readmeName);
+    initGit();
 
     console.info(`"${answers.readmeName}" is all set up!`);
 };
@@ -46,6 +48,8 @@ const createNewPackageJson = (name, repoUrl) => {
     const defaultPackageJson = path.resolve(rootDir, 'scripts', 'default-meta-files', 'package.json');
     const destinationPackageJson = path.resolve(rootDir, 'package.json');
     let packageJson = fs.readFileSync(defaultPackageJson, 'utf8');
+
+    inform(`Creating new "package.json"`);
 
     packageJson = packageJson.replace('ng2-webpack-bare', name)
         .replace('"version": "1.0.0"', '"version": "0.0.0"')
@@ -58,7 +62,46 @@ const createNewReadme = readmeTitle => {
     const destinationReadme = path.resolve(rootDir, 'README.md');
     const readme = `#${readmeTitle}`;
 
+    inform(`Creating new "README.md"`);
     fs.writeFileSync(destinationReadme, readme, { encoding: 'utf8' });
+};
+
+const initGit = () => {
+    deleteFolder(path.resolve(rootDir, '.git'), true);
+    createGitProject();
+};
+
+const deleteFolder = (folderPath, topLevel) => {
+    if (fs.existsSync(folderPath)) {
+        if (topLevel) {
+            inform(`Deleting ${folderPath}`);
+        }
+
+        fs.readdirSync(folderPath).forEach((file, index) => {
+            const filePath = path.resolve(folderPath, file);
+
+            if (fs.lstatSync(filePath).isDirectory()) {
+                deleteFolder(filePath);
+            } else {
+                fs.unlinkSync(filePath);
+            }
+        });
+
+        fs.rmdirSync(folderPath);
+    }
+};
+
+const createGitProject = () => {
+    const startingDir = __dirname;
+
+    inform(`Running \`git init\` in "${rootDir}"`);
+    process.chdir(rootDir);
+    childProcess.execSync('git init');
+    process.chdir(startingDir);
+};
+
+const inform = message => {
+    console.info('>', message);
 };
 
 console.info(`
